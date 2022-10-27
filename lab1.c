90bdddd697fca26650c849d60350b9c15187210c
AbdullahAl-rubaye <93665223+AbdullahAl-rubaye@users.noreply.github.com>
2022-10-26 23:28:15 -0400

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -2,4 +2,10 @@
  * Implement your solution in thi file
  *
  */
-
+ public static String mysteryExplosion(String word) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i <= word.length(); i++) {
+            builder.append (word,0, i);
+        }
+        return builder.toString();
+    }
